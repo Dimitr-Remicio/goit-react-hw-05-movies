27626dab54e20e45cecbec43da7438ff285c4398
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-07-08 22:27:43 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,29 @@ import { lazy, Suspense, useState } from "react";
 import {Routes, Route} from 'react-router-dom';
 import BarLoader from 'react-spinners/BarLoader';
 import './App.css';
+
 import AppBar from './AppBar/AppBar';
 
-const HomePage = lazy(() => import('../Pages/Home/Home' /* webpackChunkName: "home-page" */))
-const MoviesPage = lazy(() => import('../Pages/Movies/Movies' /* webpackChunkName: "movies-page" */))
-const MovieDetailsPage = lazy(() => import('../Pages/MovieDetails/MovieDetails' /* webpackChunkName: "movie-details-page" */))
-const Cast = lazy(() => import('./Cast/Cast' /* webpackChunkName: "cast" */))
-const Reviews = lazy(() => import('./Reviews/Reviews' /* webpackChunkName: "reviews" */))
-const Trailer = lazy(() => import('./Trailer/Trailer' /* webpackChunkName: "trailer" */))
+const HomePage = lazy(() => delayForDemo(import('../Pages/Home/Home' /* webpackChunkName: "home-page" */)))
+const MoviesPage = lazy(() => delayForDemo(import('../Pages/Movies/Movies' /* webpackChunkName: "movies-page" */)))
+const MovieDetailsPage = lazy(() => delayForDemo(import('../Pages/MovieDetails/MovieDetails' /* webpackChunkName: "movie-details-page" */)))
+const Cast = lazy(() => delayForDemo(import('./Cast/Cast' /* webpackChunkName: "cast" */)))
+const Reviews = lazy(() => delayForDemo(import('./Reviews/Reviews' /* webpackChunkName: "reviews" */)))
+const Trailer = lazy(() => delayForDemo(import('./Trailer/Trailer' /* webpackChunkName: "trailer" */)))
 
 import Notfound from './Notfound/Notfound';
 import Container from './Container/Container';
 
+function delayForDemo(promise) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 400);
+  }).then(() => promise);
+}
+
 function App() {
   
 
-  let [color, setColor] = useState("#EF6401");
+  let [color, setColor] = useState("#F34646");
 
   return (
     <> 
