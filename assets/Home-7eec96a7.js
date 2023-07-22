ea8a67f42308fa2421e53169178d4aa11b22bb67
Dimitr-Remicio <Dimitr-Remicio@users.noreply.github.com>
2023-07-22 00:26:48 +0000

    deploy: 5dce286fa2ec1d15ba156beb1cca5e4e58cd1c60

---
+++ assets/Home-7eec96a7.js
@@ -0,0 +1,1 @@
+import{r as s,j as e}from"./index-f37f0c7f.js";import{n as i,M as n,a}from"./normalized-data-09678617.js";import{f as m}from"./api-d8b78cbe.js";import"./genres-names-2136eebc.js";function u(){const[t,o]=s.useState([]);return s.useEffect(()=>{m().then(r=>{o(i(r.results))})},[]),e.jsx(n,{children:e.jsx(a,{data:t})})}function v(){return e.jsx(u,{})}export{v as default};
